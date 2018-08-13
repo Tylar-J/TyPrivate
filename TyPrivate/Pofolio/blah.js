be2e052b190bf0c0d7e201e73f6a4912602bd426
Tylar Sammone Johnson <1Sammone@gmail.com>
2018-08-13 09:17:54 -0400

    update

--- TyPrivate/Pofolio/blah.js
+++ TyPrivate/Pofolio/blah.js
@@ -1,4 +1,12 @@
-for( var i=50;i>= 1; 1--){
-  
-    console.log(i);
-  }
+function validate() {
+  var firstName = document.getElementById('firstName').value;
+  var lastName = document.getElementById('lastName').value;
+  var email = document.getElementById('email').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
+  var company = document.getElementById('company').value;
+
+  if(firstName===''){alert('First Name is required.')}
+  if(lastName===''){alert('Last Name is required.')}
+  if(firstName===''){alert('First Name is required.')}
+
+}
